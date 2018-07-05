afc33fb84356cfb9b5643f71f157f329af93cddb
Tapio Saarinen <prime@factori.se>
2018-07-05 14:50:24 +1000

    Load HEKENGA_QUEUE later - allows for .env style setups to work

--- lib/hekenga/parallel_job.rb
+++ lib/hekenga/parallel_job.rb
@@ -1,7 +1,9 @@
 require 'active_job'
 module Hekenga
   class ParallelJob < ActiveJob::Base
-    queue_as ENV["HEKENGA_QUEUE"] || :migration
+    queue_as do
+      ENV["HEKENGA_QUEUE"] || :migration
+    end
     def perform(migration_key, task_idx, ids, test_mode)
       migration = Hekenga.find_migration(migration_key)
       migration.test_mode! if test_mode
